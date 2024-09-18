df0afd9284dc14a007a94599939009cc2ca9c175
unknown <sonhoangdang0612@gmail.com>
2024-09-19 00:07:51 +0700

    feat: create Tag component

---
+++ frontend/src/components/ListTags.tsx
@@ -0,0 +1,16 @@
+import { Row, Space } from "antd";
+import Tag from "./Tag";
+
+const ListTags = () => {
+  return (
+    <Row>
+      <Space size={[6, 6]} wrap>
+        {["magenta", "red"].map((color: any) => (
+          <Tag color={color} key={color} />
+        ))}
+      </Space>
+    </Row>
+  );
+};
+
+export default ListTags;
