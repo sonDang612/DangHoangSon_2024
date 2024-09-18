cc9301f095e1d5e447bb9dd876bdcf6c845743dc
unknown <sonhoangdang0612@gmail.com>
2024-09-19 00:08:14 +0700

    feat: create Post component

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,6 +1,9 @@
-import { Card, Space, Typography, Tag, Divider } from "antd";
-import type { Post as PostType } from "../types";
+import { Card, Divider, Space, Typography } from "antd";
 import dayjs from "dayjs";
+import React from "react";
+import type { Post as PostType } from "../types";
+import ListComments from "./ListComments";
+import ListTags from "./ListTags";
 
 const { Title, Text, Paragraph } = Typography;
 
@@ -10,35 +13,37 @@ type Props = {
 
 const Post = (props: Props) => {
   const { post } = props;
-  const formattedDate = dayjs(post?.created_at).format("MMM DD, YYYY");
+
+  const formattedDate = React.useMemo(() => {
+    return dayjs(post?.created_at).format("MMM DD, YYYY");
+  }, [post?.created_at]);
 
   return (
     <Card className="border-bottom-1 border-radius-0 border-black">
       <Title className="text-center mb-2" level={1}>
         {post?.title}
       </Title>
-      <Space size={[0, 2]} direction="vertical" style={{ marginBottom: 20 }}>
-        <Text className="text-lg font-medium leading-0">
-          Author: {post?.owner}
-        </Text>
-        <Text className="text-lg font-medium leading-0">
-          Created at: {formattedDate}
-        </Text>
+      <Space
+        direction="horizontal"
+        className="w-full justify-between align-start"
+      >
+        <Space size={[0, 2]} direction="vertical" style={{ marginBottom: 20 }}>
+          <Text className="text-lg font-medium leading-0">
+            Author: {post?.owner}
+          </Text>
+          <Text className="text-lg font-medium leading-0">
+            Created at: {formattedDate}
+          </Text>
+        </Space>
+        <ListTags />
       </Space>
       <Paragraph className="text-lg font-medium text-length-1 relative">
         {post?.content}
       </Paragraph>
       <Divider />
-      <Space size={[20, 0]} direction="horizontal" className="mb-8">
-        <Text type="secondary" className="cursor-pointer font-medium">
-          5 replies
-        </Text>
-        <Text className="cursor-pointer font-medium text-medium" type="danger">
-          (The comment section can be toggled collapse/expand)
-        </Text>
-      </Space>
+      <ListComments />
     </Card>
   );
 };
 
-export default Post;
+export default React.memo(Post);
