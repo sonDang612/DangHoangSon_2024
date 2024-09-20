299f240095bc489f1809ad6218563efcad627044
unknown <sonhoangdang0612@gmail.com>
2024-09-20 18:29:09 +0700

    add setup cor frontend

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
 
 axiosInstance.interceptors.request.use(
