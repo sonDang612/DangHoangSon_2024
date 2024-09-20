1c6964d075ac274e00a765623895547c1b2b7d63
unknown <sonhoangdang0612@gmail.com>
2024-09-20 19:00:19 +0700

    change to cors lib

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './utils/global-exceptions-filter';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: ['POST', 'PUT', 'DELETE', 'GET'],
-    allowedHeaders: '*',
-    credentials: true,
-  });
+  app.use(
+    cors({
+      origin: [
+        'https://vercel.com/sondang0612s-projects/dang-hoang-son-2024-46gc',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    }),
+  );
   app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(3000);
 }
--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 axiosInstance.interceptors.request.use(
