6bbedd1452ff71f127f16397504b50851998b4da
unknown <sonhoangdang0612@gmail.com>
2024-09-20 18:02:29 +0700

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,21 @@ import { GlobalExceptionFilter } from './utils/global-exceptions-filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
+  const whitelist = ['https://dang-hoang-son-2024-46gc.vercel.app'];
+
   app.enableCors({
-    origin: 'https://dang-hoang-son-2024-46gc.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
     credentials: true,
   });
 
