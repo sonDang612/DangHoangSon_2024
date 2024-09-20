422bbc053aed26a8c70cf0af03655657afc136ed
unknown <sonhoangdang0612@gmail.com>
2024-09-20 19:05:55 +0700

    hide global exception

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { GlobalExceptionFilter } from './utils/global-exceptions-filter';
-import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(
-    cors({
-      origin: [
-        'https://vercel.com/sondang0612s-projects/dang-hoang-son-2024-46gc',
-      ],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      credentials: true,
-    }),
-  );
-  app.useGlobalFilters(new GlobalExceptionFilter());
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { bodyParser: true });
+  // app.useGlobalFilters(new GlobalExceptionFilter());
+  app.enableCors({
+    origin: '*',
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+  });
+  await app.listen(5000);
 }
 bootstrap();
