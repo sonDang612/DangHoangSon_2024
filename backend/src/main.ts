5434e50e9e958f0f0b361110d28e54ca1cf22272
unknown <sonhoangdang0612@gmail.com>
2024-09-20 19:19:14 +0700

    fix cors v2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cors from 'cors';
 import { GlobalExceptionFilter } from './utils/global-exceptions-filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true });
-  app.use(
-    cors({
-      origin: [
-        'https://dang-hoang-son-2024-46gc.vercel.app',
-        'https://dang-hoang-son-2024-46gc.vercel.app/',
-      ],
-      methods: ['POST', 'GET', 'PUT', 'DELETE'],
-    }),
-  );
+  app.enableCors({
+    origin: [
+      'https://dang-hoang-son-2024-46gc.vercel.app/',
+      'https://dang-hoang-son-2024-46gc.vercel.app',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  });
   app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(5000);
 }
