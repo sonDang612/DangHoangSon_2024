a5dabac2e443f7dd4f2f1838df1d0dabb3268873
unknown <sonhoangdang0612@gmail.com>
2024-09-20 19:10:28 +0700

    cors from nodejs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true });
   // app.useGlobalFilters(new GlobalExceptionFilter());
-  app.enableCors({
-    origin: '*',
-    methods: ['POST', 'PUT', 'DELETE', 'GET'],
-  });
+  app.use(
+    cors({
+      origin: 'https://dang-hoang-son-2024-46gc.vercel.app',
+      methods: ['POST', 'GET', 'PUT', 'DELETE'],
+    }),
+  );
   await app.listen(5000);
 }
 bootstrap();
