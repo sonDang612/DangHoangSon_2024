5e925c2173443996621ef097d51709e4522d91b1
unknown <sonhoangdang0612@gmail.com>
2024-09-20 19:13:40 +0700

    toggle global filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cors from 'cors';
+import { GlobalExceptionFilter } from './utils/global-exceptions-filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true });
-  // app.useGlobalFilters(new GlobalExceptionFilter());
   app.use(
     cors({
       origin: 'https://dang-hoang-son-2024-46gc.vercel.app',
       methods: ['POST', 'GET', 'PUT', 'DELETE'],
     }),
   );
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(5000);
 }
 bootstrap();
--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -2,13 +2,13 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 axiosInstance.interceptors.request.use(
   function (config: any) {
+    config.headers = {
+      authorization: `Bear`,
+    };
     return config;
   },
   function (error) {
