a907146dee448d222711c7e928654d3c05d09925
unknown <sonhoangdang0612@gmail.com>
2024-09-20 13:20:58 +0700

    feat: add global exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './utils/global-exceptions-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
